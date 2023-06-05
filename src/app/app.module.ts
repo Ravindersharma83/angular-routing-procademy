e74f2ed8801dbcd783dd7c698979fc73576d32bc
ravindersharma83 <ravinder200198@gmail.com>
2023-06-05 12:42:25 +0530

    setup basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CourseComponent } from './course/course.component';
+
+const appRoute: Routes = [
+  // {path:'Home', component: HomeComponent},
+  {path:'', redirectTo : 'Home' , pathMatch:'full'},
+  {path:'About', component: AboutComponent},
+  {path:'Contact', component: ContactComponent},
+  {path:'Course', component: CourseComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoute),
   ],
   providers: [],
   bootstrap: [AppComponent]
