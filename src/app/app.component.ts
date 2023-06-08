7235f27c82656ba830e5ea3ffc5a70b427d3e336
ravindersharma83 <ravinder200198@gmail.com>
2023-06-08 18:25:32 +0530

    implementing routerEvent for maintaining loading state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, Event, NavigationStart, NavigationEnd, NavigationCancel, NavigationError  } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +7,29 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private activatedRoute:ActivatedRoute){}
+  constructor(private activatedRoute:ActivatedRoute, private router : Router){}
+
+  displayLoadingIndicator:boolean = false;
+
   ngOnInit(): void {
     this.activatedRoute.fragment.subscribe((value)=>{
       console.log(value);
       this.jumpTo(value);
-      
     })
+
+    this.router.events.subscribe((routerEvent:Event)=>{
+      if(routerEvent instanceof NavigationStart){
+        this.displayLoadingIndicator = true;
+      }
+      if(routerEvent instanceof NavigationEnd || routerEvent instanceof NavigationCancel || routerEvent instanceof NavigationError){
+        this.displayLoadingIndicator = false;
+      }
+    });
   }
 
   jumpTo(section : any){
     document.getElementById(section)?.scrollIntoView({behavior:'smooth'});
   }
+
   title = 'angular-routing-procademy';
 }
